9f0271b9e2ac9b658a49c09cd5a10905c829e86e
Nathan <nathanstouffer1999@gmail.com>
2020-11-01 10:02:55 -0700

    moving gifs directory

--- project/implementation/output/make-gif.py
+++ project/implementation/output/make-gif.py
@@ -24,6 +24,6 @@ images.append(imageio.imread(dir + "/converged.png"))
 images.append(imageio.imread(dir + "/converged.png"))
 images.append(imageio.imread(dir + "/converged.png"))
 
-file_name = "../gifs/" + dir.split("/")[0] + ".gif"
+file_name = "../../gifs/" + dir.split("/")[0] + ".gif"
 subprocess.run(['rm', file_name])
 imageio.mimsave(file_name, images, duration=1.5)
